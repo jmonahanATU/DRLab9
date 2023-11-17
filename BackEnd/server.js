7bed8e31be40e729cd697ff3d2aa6d07d5dfc138
Josh <g00409871@atu.ie>
2023-11-17 12:35:06 +0000

    code complete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4000
 const cors = require('cors');
 
-
+// Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
 app.use(function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
@@ -12,72 +12,68 @@ res.header("Access-Control-Allow-Headers",
 "Origin, X-Requested-With, Content-Type, Accept");
 next();
 });
-
+// Middleware for parsing JSON and URL-encoded data
 const bodyParser = require("body-parser");
-
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// getting-started.js
+// Connecting to MongoDB using Mongoose
+const mongoose = require('mongoose');
 
+main().catch(err => console.log(err));
+
+async function main() {
+  // Use the line below if your database requires authentication
+  // await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');
+  await mongoose.connect('mongodb+srv://admin:admin@cluster0.hcqirr6.mongodb.net/MYDB?retryWrites=true&w=majority');
+  
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+// Define a schema for the "books" collection
+const bookSchema = new mongoose.Schema({
+  title: String,
+  cover: String,
+  author: String
+});
+
+// Create a model based on the schema
+const bookModel = mongoose.model('joshs_books', bookSchema);
+
+// Handle POST requests to create a new book
 app.post('/api/book', (req,res)=>{
     console.log(req.body);
-    res.send("Book Created")
+    bookModel.create({
+      title:req.body.title,
+      cover:req.body.cover,
+      author:req.body.author
+    })
+    .then(()=>{res.send("Book Created")})
+    .catch(()=>{res.send("Book NOT Created")})
 })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+// Handle GET requests to retrieve all books
+app.get('/api/books', async(req, res)=>{
 
-app.get('/api/books', (req, res)=>{
-    
-const data = [
-    {
-    "title": "Learn Git in a Month of Lunches",
-    "isbn": "1617292419",
-    "pageCount": 0,
-    "thumbnailUrl":
-    "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg", "status": "MEAP",
-    "authors": ["Rick Umali"],
-    "categories": []
-    },
-    {
-    "title": "MongoDB in Action, Second Edition",
-    "isbn": "1617291609",
-    "pageCount": 0,
-    "thumbnailUrl":
-    "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-    "status": "MEAP",
-    "authors": [
-    "Kyle Banker",
-    "Peter Bakkum",
-    "Tim Hawkins",
-    "Shaun Verch",
-    "Douglas Garrett"
-    ],
-    "categories": []
-    },
-    {
-    "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-    "isbn": "1617292036",
-    "pageCount": 0,
-    "thumbnailUrl":
-    "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-    "status": "MEAP",
-    "authors": ["Simon Holmes"],
-    "categories": []
-    }
-    ];
+  let books = await bookModel.find({});
+  res.json(books);
 
-    res.json({
-        myBooks:data,
-        "MyMessage":"Hello Data"
-    }) 
+})
+// Handle GET requests to retrieve a specific book by ID
+app.get('/api/book/:id', async(req,res)=> {
+  console.log(req.params.id);
 
+  let book = await bookModel.findById(req.params.id);
+  res.send(book);
 })
 
 
-
+// Start the Express app and listen on the specified port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- src/components/read.js
+++ src/components/read.js
@@ -1,36 +1,41 @@
+// Importing necessary React hooks and external libraries
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Books from "./books";
 
+// Functional component named Read
 function Read() {
-   
+    // State variable to store data fetched from the server
     const [data, setData] = useState([]);
 
-  useEffect(
-    ()=>{
-        
+    // useEffect hook is used for handling side effects in functional components
+    useEffect(() => {
+        // Axios is used to make HTTP requests
         axios.get('http://localhost:4000/api/books')
-        .then(
-            (response)=>{
-                setData(response.data.myBooks)
-            }
-        )
-        .catch(
-            (error)=>{
-                console.log(error);
-            }
-        )
+            .then(
+                // If the request is successful, update the state with the fetched data
+                (response) => {
+                    setData(response.data)
+                }
+            )
+            .catch(
+                // If there is an error, log it to the console
+                (error) => {
+                    console.log(error);
+                }
+            )
 
-    }, []
-  );
+    }, []); // The empty dependency array ensures that this effect runs once after the initial render
 
+    // JSX to render the component
     return (
         <div>
             <h2>Hello from Read Component!</h2>
+            {/* Rendering the Books component and passing the fetched data as a prop */}
             <Books myBooks={data}></Books>
         </div>
     );
-
 }
 
+// Exporting the Read component to make it available for other components
 export default Read;
